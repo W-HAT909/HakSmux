a32e2d758b505fd04c3c078c7b7548a5ff6e34e7
ANO-W909 <oniontor19@gmail.com>
2024-09-08 22:24:32 +0800

    Add files via upload

---
+++ HakSmux.py
@@ -0,0 +1,9 @@
+# This For Setting HakSmux
+import os
+
+os.system("clear")
+os.system("pkg install jp2a")
+os.system("rm $HOME/../usr/etc/bash.bashrc")
+os.system("mv bash.bashrc $HOME/../usr/etc")
+os.system("mkdir $HOME/../usr/etc/HakSmuX")
+os.system("mv * $HOME/../usr/etc/HakSmuX")
